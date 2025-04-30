faf03cf2f2817f2362ca596b9243407ece49f414
JoshuaHoldenried <joshua.holdenried@gmail.com>
2025-04-29 17:51:40 -0700

    Fan Fully Pushable / Sand Friction Material

--- Assets/Scripts/FanController.cs
+++ Assets/Scripts/FanController.cs
@@ -3,30 +3,58 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+/* Joshua Holdenried
+ * This script allows the golf ball to be shot in any direction by the fan
+ * First Updated: 4/29/25
+ * Last Updated: 4/29/25
+ */
+
 public class FanController : MonoBehaviour
 {
 
     public Rigidbody ballRb;
     public float FanSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public bool up = false;
+    public bool down = false;
+    public bool left = false;
+    public bool right = false;
 
     void OnTriggerStay(Collider other)
     {
-        if (ballRb)
+
+        if (up is true)
         {
-            ballRb.AddForce(Vector3.up * FanSpeed);
+            if (ballRb)
+            {
+                ballRb.AddForce(Vector3.up * FanSpeed);
+            }
+        }
+
+        if (down is true)
+        {
+            if (ballRb)
+            {
+                ballRb.AddForce(Vector3.down * FanSpeed);
+            }
+        }
+
+        if (left is true)
+        {
+            if (ballRb)
+            {
+                ballRb.AddForce(Vector3.left * FanSpeed);
+            }
+        }
+
+        if (right is true)
+        {
+            if (ballRb)
+            {
+                ballRb.AddForce(Vector3.right * FanSpeed);
+            }
         }
     }
+
+
 
 }
