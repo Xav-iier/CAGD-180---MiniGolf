ba01621052405d17a8ce40bda70759b44f90a6f2
JoshuaHoldenried <joshua.holdenried@gmail.com>
2025-05-08 17:56:30 -0700

    Slight Color Picker Changes

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +8,22 @@ using UnityEngine.SceneManagement;
 /* Joshua Holdenried
  * This script allows the player to change the color of their golf ball
  * First Updated: 5/6/25
- * Last Updated: 5/6/25
+ * Last Updated: 5/8/25
  */
+
+public enum BallColor
+{
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    purple,
+    pink,
+    white,
+    gray,
+    black
+}
 
 public class ColorChanger : MonoBehaviour
 {
@@ -23,16 +38,10 @@ public class ColorChanger : MonoBehaviour
     public Color ColorWhite;
     public Color ColorGray;
     public Color ColorBlack;
-    public bool red = false;
-    public bool orange = false;
-    public bool yellow = false;
-    public bool green = false;
-    public bool blue = false;
-    public bool purple = false;
-    public bool pink = false;
-    public bool white = false;
-    public bool gray = false;
-    public bool black = false;
+
+    public BallColor ballColor;
+    public Transform GolfBallLocation;
+
     public void MainMenu(int sceneIndex)
     {
         SceneManager.LoadScene(1);
@@ -43,136 +52,58 @@ public class ColorChanger : MonoBehaviour
 
         if (objs.Length > 1)
         {
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
 
-        DontDestroyOnLoad(gameObject);
+        transform.position = GolfBallLocation.position;
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        switch (ballColor)
+        {
+            case BallColor.red:
+                Red();
+                break;
+            case BallColor.orange:
+                Orange();
+                break;
+            case BallColor.yellow:
+                Yellow();
+                break;
+            case BallColor.green:
+                Green();
+                break;
+            case BallColor.blue:
+                Blue();
+                break;
+            case BallColor.purple:
+                Purple();
+                break;
+            case BallColor.pink:
+                Pink();
+                break;
+            case BallColor.white:
+                White();
+                break;
+            case BallColor.gray:
+                Gray();
+                break;
+            case BallColor.black:
+                Black();
+                break;
+            default:
+                //do code if enum is not any of the above cases
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(red = true)
-        {
-            orange = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (orange = true)
-        {
-            red = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (yellow = true)
-        {
-            red = false;
-            orange = false;
-            green = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (green = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (blue = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            green = false;
-            purple = false;
-            pink = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (purple = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            pink = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (pink = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            purple = false;
-            white = false;
-            gray = false;
-            black = false;
-        }
-        if (white = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            gray = false;
-            black = false;
-        }
-        if (gray = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            white = false;
-            black = false;
-        }
-        if (black = true)
-        {
-            red = false;
-            orange = false;
-            yellow = false;
-            green = false;
-            blue = false;
-            purple = false;
-            pink = false;
-            white = false;
-            gray = false;
-        }
-
+      
     }
 
 
@@ -185,7 +116,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorRed;
-                red = true;
             }
             else
             {
@@ -202,7 +132,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorOrange;
-                orange = true;
             }
             else
             {
@@ -219,7 +148,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorYellow;
-                yellow = true;
             }
             else
             {
@@ -236,7 +164,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorGreen;
-                green = true;
             }
             else
             {
@@ -253,7 +180,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorBlue;
-                blue = true;
             }
             else
             {
@@ -270,7 +196,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorPurple;
-                purple = true;
             }
             else
             {
@@ -287,7 +212,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorPink;
-                pink = true;
             }
             else
             {
@@ -304,7 +228,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorWhite;
-                white = true;
             }
             else
             {
@@ -321,7 +244,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorGray;
-                gray = true;
             }
             else
             {
@@ -338,7 +260,6 @@ public class ColorChanger : MonoBehaviour
             if (targetRenderer != null && targetRenderer.material != null)
             {
                 targetRenderer.material.color = ColorBlack;
-                black = true;
             }
             else
             {
