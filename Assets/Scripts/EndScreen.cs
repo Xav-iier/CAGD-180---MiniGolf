42641f4afc82ad972d612ccfd9ce4c2f77102187
JoshuaHoldenried <joshua.holdenried@gmail.com>
2025-05-11 17:35:25 -0700

    Tuning Everything Up

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -15,32 +15,24 @@ public class EndScreen : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    public void Again(int sceneIndex)
+      public void LevelSelect(int sceneIndex)
     {
         SceneManager.LoadScene(1);
     }
-    public void LevelSelect(int sceneIndex)
+    public void Grass(int sceneIndex)
     {
         SceneManager.LoadScene(2);
     }
-    public void Grass(int sceneIndex)
+    public void Desert(int sceneIndex)
     {
         SceneManager.LoadScene(3);
     }
-    public void Desert(int sceneIndex)
-    {
-        SceneManager.LoadScene(4);
-    }
     public void School(int sceneIndex)
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(4);
     }  public void Controls(int sceneIndex)
     {
-        SceneManager.LoadScene(6);
+        SceneManager.LoadScene(5);
     }
-    public void colorpicker(int sceneIndex)
-    {
-        SceneManager.LoadScene(7);
-    }
-
+   
 }
